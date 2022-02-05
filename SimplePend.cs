4e342f6718c9b9b24933d21fa996dfb48af28c35
David Schmidt <david.schmidt67.niu@gmail.com>
2022-02-04 19:50:31 -0600

    RK4 is now working

--- SimplePend.cs
+++ SimplePend.cs
@@ -46,6 +46,50 @@ namespace sim
         }
 
         //--------------------------------------------------------------------
+        //rk4: Performs ONE integration step via RK4 Method.
+        //--------------------------------------------------------------------
+        public void rk4(double dt)
+        {
+            double[,] sl = new double[4,2] {{0.0 , 0.0} , {0.0 , 0.0} , {0.0 , 0.0} , {0.0 , 0.0}};
+            double[] xi = new double[n];
+
+            int i;
+            //Solving for kA, kB, kC, and kD
+            rhsFunc(x, f);
+            for(i=0; i<n; ++i)
+            {
+                sl[0,i] = f[i];
+                xi[i] = x[i] + sl[0,i]*0.5*dt;
+            }
+
+            rhsFunc(xi, f);
+            for(i=0; i<n; ++i)
+            {
+                sl[1,i] = f[i];
+                xi[i] = x[i] + sl[1,i]*0.5*dt;
+            }
+
+            rhsFunc(xi, f);
+            for(i=0; i<n; ++i)
+            {
+                sl[2,i] = f[i];
+                xi[i] = x[i] + sl[2,i]*dt;
+            }
+
+            rhsFunc(xi, f);
+            for(i=0; i<n; ++i)
+            {
+                sl[3,i] = f[i];
+                xi[i] = x[i] + sl[3,i]*0.5*dt;
+            }
+
+            for(i=0; i<n; ++i)
+            {
+                x[i] = x[i] + (sl[0,i] + 2*sl[1,i] + 2*sl[2,i] + sl[3,i])*dt/6;
+            }
+        }
+
+        //--------------------------------------------------------------------
         //Getters and Setters Methods
         //--------------------------------------------------------------------
         public double L
